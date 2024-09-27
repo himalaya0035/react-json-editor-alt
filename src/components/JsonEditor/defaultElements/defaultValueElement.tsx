e1647748c6c12b3c1146f7bf583844b5c3eff815
himalaya0035 <guptahimalaya2@gmail.com>
2024-09-28 01:02:38 +0530

    add different formatting styles for different types of value in defaultValueElement

--- src/components/JsonEditor/defaultElements/defaultValueElement.tsx
+++ src/components/JsonEditor/defaultElements/defaultValueElement.tsx
@@ -6,9 +6,32 @@ import { INLINE_EDITING_MODE } from "../../../constants/constants";
 function DefaultValueElement({ value, path, isFieldPresentInNonEditableLookup}: DefaultValueElementProps) {
   const { editingMode} = useJsonEditorContext();
 
+  const getFormattedContent = () => {
+    if (value === undefined) {
+      return { text: 'Undefined', style: 'text-gray-500' }; 
+    }
+    if (value === null) {
+      return { text: 'Null', style: 'text-rose-500' }; 
+    }
+    if (value === true) {
+      return { text: 'True', style: 'text-emerald-500' }; 
+    }
+    if (value === false) {
+      return { text: 'False', style: 'text-amber-500' }; 
+    }
+    if (value === '') {
+      return { text: 'Empty', style: 'text-indigo-500' };
+    }
+    return { text: value, style : ''}; 
+  };
+
+  const { text, style } = getFormattedContent();
+
   return (
     <>
-      <span>{value}</span>
+      <span className={`${style}`}>
+        {text}
+      </span>
       {(editingMode === INLINE_EDITING_MODE && !isFieldPresentInNonEditableLookup) && (
         <InlineEditButton path={path} />
       )}
