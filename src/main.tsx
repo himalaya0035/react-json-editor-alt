d6cb2213e7d7c7d6f626529841a91f3f0d018e25
himalaya0035 <guptahimalaya2@gmail.com>
2024-10-05 21:53:55 +0530

    render examples instead of app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import Example from './examples/examples.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Example />
   </StrictMode>,
 )
